4d2a44a710ae66204017714f9dc1e24d0a91bb22
andiwinata <andi.winata@hotmail.com>
2018-11-24 16:19:03 +1100

    Adding auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,21 @@ import './styles/stylesheets.scss';
 import './App.scss';
 import Header from './components/Header';
 import Home from './components/Home';
-import Login from './components/LoginForm'
 import DonorList from './components/DonorList'
+import Auth from './components/Auth';
 
 class App extends Component {
   render() {
     return (
       <div className="c-app">
         <Header />
-        <BrowserRouter>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-          </Switch>
-        </BrowserRouter>
-        <DonorList />
+        <Auth>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </BrowserRouter>
+        </Auth>
       </div>
     );
   }
