1f1f077485f3c542fa6eb486d44abf322ab2cfe1
LpmRaven <liamraven@gmail.com>
2018-11-25 10:55:53 +1100

    updates lambda

--- backend/mailchimp-dividend-impact-statement-lambda/handler.js
+++ backend/mailchimp-dividend-impact-statement-lambda/handler.js
@@ -1,36 +1,67 @@
 const AWS = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+const MailchimpAPI = require('mailchimp-api-v3');
 
 AWS.config.update({
     region: "ap-southeast-2",
 });
 
-const dynamoDBParams = {
-    ExpressionAttributeValues: {
-        ":topic": {
-            S: "PHRASE"
-        }
-    },
-    FilterExpression: "contains (Subtitle, :topic)",
-    ProjectionExpression: "Title, Subtitle",
-    TableName: "EPISODES_TABLE"
-};
+const mailchimpAPIKey = 'fed93f27524a7c6378592543595f346a-us19';
+const mailchimp = new MailchimpAPI(mailchimpAPIKey);
+
+const mailchimpRequest = (method, endpoint, pathParams, body, query) => {
+    return mailchimp.request({
+        method,
+        path: endpoint,
+        path_params: pathParams,
+        body,
+        query
+    }, callback);
+}
 
 exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
 
-    dynamodb.scan(dynamoDBParams, (err, data) => {
-        if (err) {
-            console.log("Error", err);
-        } else {
-            data.Items.forEach((element, index, array) => {
-                console.log(element.Title.S + " (" + element.Subtitle.S + ")");
-            });
+    try {
+        const listsRequestPathParams = '';
+        const listsRequestBody = {
+            name: 'test list',
+            contact: {
+                company: 'Investing for charity',
+                address: 'abc',
+                city: 'Sydney',
+                state: 'NSW',
+                zip: '2000',
+                country: 'Australia'
+            },
+            permission_reminder: false,
+            campaign_defaults: false
         }
-    });
 
-    return response;
+        let responseOne = await mailchimpRequest('POST', '/lists', listsRequestPathParams, listsRequestBody);
+
+        // get list id
+
+        // let responseTwo = await mailchimpRequest('POST', '/');
+        // let responseThree = await mailchimpRequest('POST');
+
+        // lists
+
+        // list id, manage field
+
+        // members
+
+        // create campaign
+
+        // campaign send
+
+        const res = responseOne;
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify('Hello from Lambda!', res),
+        };
+    }
+    catch(error) {
+        return error;
+    }
+
 };
